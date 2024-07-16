d408b2e0eaf3ee56ba5552a3679d42e9fa50a506
Xander Swan <xanderswan32@gmail.com>
2024-07-16 11:19:24 -0400

    Redirect `print()` to `log.info()`

--- log.lua
+++ log.lua
@@ -1,7 +1,9 @@
 local log = {}
 
+local oldPrint = print
+
 local function msg(...)
-  print(...)
+  oldPrint(...)
 end
 
 function log.info(...)
@@ -15,6 +17,10 @@ end
 function log.fatal(...)
   msg("[FATAL]", ...)
   error(...)
+end
+
+function print(...)
+  log.info(...)
 end
 
 return log
