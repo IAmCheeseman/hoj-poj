3a67302fbaf440bfcdb3fa9560b10690f0062db4
Xander Swan <xanderswan32@gmail.com>
2024-07-17 16:18:12 -0400

    Flash shader <@1112843835643543612>

--- objects/dummy.lua
+++ objects/dummy.lua
@@ -6,6 +6,8 @@ local object = require("object")
 
 local Dummy = object()
 
+local flash = love.graphics.newShader("vfx/flash.frag")
+
 function Dummy:init(x, y)
   self.sprite = Sprite("assets/dummy.ase")
   self.sprite:alignedOffset("center", "bottom")
@@ -13,17 +15,9 @@ function Dummy:init(x, y)
   self.x = x
   self.y = y
 
-  self.velx = 0
-  self.vely = 0
-
-  self.hp = 20
+  self.iframes = 0
 
   self.recentDamage = 0
-
-  self.body = core.ResolverBody(self, core.physics.rect(-6, -5, 11, 5), {
-    layers = {"env"},
-  })
-  core.physics.world:addBody(self.body)
 
   self.hitbox = core.SensorBody(self, core.physics.rect(-6, -15, 11, 15), {
     layers = {"enemy"},
@@ -37,32 +31,26 @@ function Dummy:removed()
 end
 
 function Dummy:takeDamage(amount, kbDirX, kbDirY)
-  self.hp = self.hp - amount
-  if self.hp <= 0 then
-    core.world:remove(self)
-  end
   self.recentDamage = amount
-
-  self.velx = self.velx + kbDirX
-  self.vely = self.vely + kbDirY
+  self.iframes = 0.1
 end
 
 function Dummy:update(dt)
-  self.velx = core.math.dtLerp(self.velx, 0, 15)
-  self.vely = core.math.dtLerp(self.vely, 0, 15)
-
-  self.velx, self.vely = self.body:moveAndCollide(self.velx, self.vely)
-
   self.zIndex = self.y
-
+  self.iframes = math.max(self.iframes - dt, 0)
   shadow.queueDraw(self.sprite, self.x, self.y, self.scalex, 1)
 end
 
 function Dummy:draw()
   love.graphics.setColor(1, 1, 1)
-  self.sprite:draw(self.x, self.y)
 
-  love.graphics.print(self.hp, self.x, self.y)
+  local scaley = 1 + self.iframes * 2
+  local scalex = 1 - (scaley - 1)
+
+  flash:send("amount", math.ceil(self.iframes))
+  love.graphics.setShader(flash)
+  self.sprite:draw(self.x, self.y, 0, scalex, scaley)
+  love.graphics.setShader()
 end
 
 TiledMap.s_addSpawner("Dummy", function(world, data)
