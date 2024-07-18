ae447eb08838aa4dd879101d4db6aa6bcae9d6fc
Xander Swan <xanderswan32@gmail.com>
2024-07-18 16:26:29 -0400

    Forgot to change window size

--- conf.lua
+++ conf.lua
@@ -3,11 +3,11 @@ local metaInfo = require("meta_info")
 function love.conf(t)
   t.window.title = metaInfo.name .. " " .. metaInfo.version.str
   t.window.icon = nil
-  t.window.width = 320 * 3
+  t.window.width = 240 * 3
   t.window.height = 180 * 3
   t.window.borderless = false
   t.window.resizable = true
-  t.window.minwidth = 320 * 3
+  t.window.minwidth = 240 * 3
   t.window.minheight = 180 * 3
   t.window.fullscreen = false
   t.window.fullscreentype = "desktop"
